1de27cb7612105da0a37c0940970f990a7c34448
krozhkov09@gmail.com <krozhkov09@gmail.com>
2020-11-30 21:17:53 +0200

    hw2 new try again

--- src/main/java/homework2/SeaBattle.java
+++ src/main/java/homework2/SeaBattle.java
@@ -4,35 +4,52 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SeaBattle {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
         System.out.println("All set. Get ready to rumble!");
-
-           public static void print (char[][] generateField){
-                System.out.println(Arrays.deepToString(generateField));
-            }
-//         generateField();
-
+         generateField();
+         editField();
         }
     public static void generateField() {
-                char[][] field = new char[5][5];
-                for (char[] chars : field) {
-                    Arrays.fill(chars, '-');
-                }
-                System.out.println("\t" + 1 + "\t" + 2 + "\t" + 3 + "\t" + 4 + "\t" + 5);
-
-                for (int i = 0; i < 5; i++) {
-                    System.out.print(i + 1 + "\t");
-                    for (int j = 0; j < 5; j++) {
-                        System.out.print(field[i][j] + "\t");
-                    }
-                }
+      char [][]  field = new char[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                field[i][j] = '_';
+            }
+        }
+        System.out.println("\t" + 1 + "\t" + 2 + "\t" + 3 + "\t" + 4 + "\t" + 5);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + 1 + "\t");
+            for (int j = 0; j < 5; j++) {
+                System.out.print(field[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
-    public static void editField (generateField, int targetRow, int targetColumn){
+    public static void print (char[][] generateField){
+        System.out.print(Arrays.deepToString(generateField));
+    }
+    public static void editField (char generateField, int targetRow, int targetColumn){
         Random random = new Random();
         targetRow = random.nextInt(4);
         targetColumn = random.nextInt(4);
-        int[][] target = new int[2][2];
-
+       int userRow;
+       int userColumn;
+      do {
+          Scanner in = new Scanner(System.in);
+          System.out.println("Enter your number of row from 1 to 5: ");
+           userRow = in.nextInt();
+          System.out.println("Enter your number of column from 1 to 5: ");
+          userColumn = in.nextInt();
+          System.out.println(userColumn);
+          if (userRow - 1 != targetRow & userColumn -1 != targetColumn){
+              System.out.println("You missed. Try again");
+          } else if (userRow - 1 != targetRow & userColumn -1 == targetColumn){
+              System.out.println("You missed. Try again");
+          }else {
+              System.out.println("You won!");
+          }
+      } while ( targetRow  ==  userRow & targetColumn ==  userColumn );
     }
 }
