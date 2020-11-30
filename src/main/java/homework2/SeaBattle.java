e704bc4712ee1c84544dcddd8f0caef3045ab3a5
krozhkov09@gmail.com <krozhkov09@gmail.com>
2020-11-30 12:36:45 +0200

    hw2 new try

--- src/main/java/homework2/SeaBattle.java
+++ src/main/java/homework2/SeaBattle.java
@@ -7,25 +7,32 @@ public class SeaBattle {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("All set. Get ready to rumble!");
-        char[][] field = new char[5][5];
-        for (char[] chars : field) {
-            Arrays.fill(chars, '-');
-        }
-        System.out.println("\t" + 1 + "\t" + 2 + "\t" + 3 + "\t" + 4 + "\t" + 5);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print(i + 1 + "\t");
-            for (int j = 0; j < 5; j++) {
-                System.out.print(field[i][j] + "\t");
+           public static void print (char[][] generateField){
+                System.out.println(Arrays.deepToString(generateField));
             }
-            System.out.println();
-        }
-
-            Random random = new Random();
-            int targetRow = random.nextInt(4);
-            int targetColumn = random.nextInt(4);
-            int[][] target = new int[1][1];
+//         generateField();
 
         }
+    public static void generateField() {
+                char[][] field = new char[5][5];
+                for (char[] chars : field) {
+                    Arrays.fill(chars, '-');
+                }
+                System.out.println("\t" + 1 + "\t" + 2 + "\t" + 3 + "\t" + 4 + "\t" + 5);
+
+                for (int i = 0; i < 5; i++) {
+                    System.out.print(i + 1 + "\t");
+                    for (int j = 0; j < 5; j++) {
+                        System.out.print(field[i][j] + "\t");
+                    }
+                }
+    }
+    public static void editField (generateField, int targetRow, int targetColumn){
+        Random random = new Random();
+        targetRow = random.nextInt(4);
+        targetColumn = random.nextInt(4);
+        int[][] target = new int[2][2];
 
     }
+}
